05b7fb49f3c9c2aec7108deffa77f0ac1aeb13f3
Harshit Choudhary <harshit1840077@gmail.com>
2023-11-21 01:34:16 +0530

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { S1Service } from './s1.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ShoesEcom';
+  shoesData: any[] = [];
+
+  constructor(private service: S1Service) { }
+
+  ngOnInit(): void {
+    this.service.getData().subscribe(
+      (data: any) => {
+        if (data && data.sneakers && Array.isArray(data.sneakers)) {
+          this.shoesData = data.sneakers;
+          console.log(this.shoesData);
+        } else {
+          console.error('Invalid data format. Expected an object with a "sneakers" property.');
+        }
+      },
+      (error) => {
+        console.error('Error in AppComponent:', error);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule  ,
+    MatCardModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
