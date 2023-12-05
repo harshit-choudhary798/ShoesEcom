d515c1207970bec95f46ad3878f4d1041b51df63
Harshit Choudhary <harshit1840077@gmail.com>
2023-12-05 20:58:53 +0530

    logics for cart items & amount  in all components

--- src/app/s1.service.ts
+++ src/app/s1.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, Subject, ReplaySubject } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class S1Service {
 
+
   private url = 'https://shoes-api.pages.dev/api.json';
+
+  itemCount = new BehaviorSubject(0);
+
+  cartItemsSubject = new BehaviorSubject<Array<any>>([]);
+  Updatedcount: any;
+  countValues:any;
+  
 
   constructor(private http: HttpClient) { }
 
@@ -19,8 +27,56 @@ export class S1Service {
   getProduct(id: number): Observable<any> {
     return this.getData().pipe(
       map((data: any) => {
-        return data.sneakers && data.sneakers.find((item:any) => item.id === id);
+        return data.sneakers && data.sneakers.find((item: any) => item.id === id);
       })
     );
   }
-}
+
+  addToCart(id: any) {
+    this.getProduct(id).subscribe(productToAdd => {
+      const currentCartItems = this.cartItemsSubject.getValue();
+      
+     
+      const existingItemIndex = currentCartItems.findIndex(item => item.id === productToAdd.id);
+  
+      if (existingItemIndex !== -1) {
+        
+        currentCartItems[existingItemIndex].quantity += 1;
+      } else {
+        
+        currentCartItems.push({ ...productToAdd, quantity: 1 });
+      }
+      this.cartItemsSubject.next([...currentCartItems]);
+    });
+  }
+
+  updateCart(items: any, quantity: number) {
+    const updatedItems = this.cartItemsSubject.getValue().filter((item: any) => {
+      return item.id !== items.id;
+    });
+  
+   
+    this.cartItemsSubject.next(updatedItems);
+  
+    
+    const totalCount = updatedItems.reduce((total, item) => total + item.quantity, 0);
+  
+
+    this.itemCount.next(totalCount);
+  }
+  
+  
+
+  }  
+
+
+  
+
+
+  
+  
+  
+  
+  
+  
+
