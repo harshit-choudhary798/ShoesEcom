d0a74e6630a54a1f2352e1d3a631fa7e3f1600b7
Harshit Choudhary <harshit1840077@gmail.com>
2023-12-05 20:53:45 +0530

    navbar amount changing

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { S1Service } from './../s1.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _service:S1Service) { }
+
+  count:any=0
 
   ngOnInit(): void {
+    this._service.itemCount.subscribe((res)=>{
+      this.count=res
+    })
   }
+
+
+
 
 }
