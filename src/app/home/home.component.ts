858056b37b8e63b7b71b0809b93ab73342f6c600
Harshit Choudhary <harshit1840077@gmail.com>
2023-12-05 21:08:48 +0530

    added category section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { S1Service } from '../s1.service';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarRef, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,14 @@ import { S1Service } from '../s1.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
   title = 'ShoesEcom';
   shoesData: any[] = [];
+  i:any
+  // horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  // verticalPosition: MatSnackBarVerticalPosition = 'top';
 
-  constructor(private service: S1Service) { }
+  constructor(private service: S1Service,private _snackBar: MatSnackBar,private router: Router) { }
 
   ngOnInit(): void {
     this.service.getData().subscribe(
@@ -26,6 +32,32 @@ export class HomeComponent implements OnInit {
         console.error('Error in AppComponent:', error);
       }
     );
+    
+    this.service.itemCount.subscribe(res => this.i = res);
   }
+  addItem(id: any) {
+    console.log(id);
+    this.service.addToCart(id);
+
+    // Check if this.i is undefined before incrementing
+    if (this.i === undefined) {
+      this.i = 1;
+    } else {
+      this.i++;
+    }
+
+    this.service.itemCount.next(this.i);
+
+    // Show Snack Bar
+    const durationMilliseconds = 1000;
+    const snackBarRef: MatSnackBarRef<any> = this._snackBar.open("Item has been added to the cart", 'View items', {
+      duration: durationMilliseconds,
+    });
+
+    snackBarRef.onAction().subscribe(() => {
+      this.router.navigate(['/cart']);
+    });
+  }
+  
 
 }
